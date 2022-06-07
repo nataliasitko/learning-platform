92cc070bd4476b6a8e404c8f80f93d8419ec51be
Natalia Sitko~ <nataliasitko@interia.pl>
2022-06-07 15:42:59 +0200

    Add web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminCtrl;
-use App\Http\Controllers\SetController;
+use App\Http\Controllers\Sets\SetController;
+use App\Http\Controllers\UserController;
+use App\Http\Requests\CreateSetRequest;
 use http\Client\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,20 +28,35 @@ Route::get('/dashboard', function () {
 
 //Route::get('/',[SetController::class,'showSets'])->name('showSets');
 
-Route::middleware('auth','admin') -> name('sets.') -> prefix('sets') -> group(function(){
-    Route::get('/',[AdminCtrl::class,'showSets'])->name('showSets'); //admin.index
+Route::middleware('auth','admin') -> name('admin.') -> prefix('admin')->middleware(['auth', 'password.confirm'])-> group(function(){
+    Route::get('/',[AdminCtrl::class,'index'])->name('index'); //admin.index
 });
 
 Route::post('/set', function (Request $request) {
     //TODO post set
 });
 
-Route::delete('/set/{id}', function ($id) {
+Route::delete('/show/{id}', function ($id) {
     //todo delete set
 });
 
-Route::middleware('auth','admin') -> name('admin.') -> prefix('admin') -> group(function(){
-    Route::get('/',[AdminCtrl::class,'index'])->name('index'); //admin.index
+Route::middleware('auth') -> name('sets.') -> prefix('sets') -> group(function(){
+    Route::get('/',[SetController::class,'index'])->name('index');
+    Route::get('/create',[SetController::class,'create'])->name('create');
+    Route::get('/name',[SetController::class,'name'])->name('name');
+    Route::post('store', [SetController::class, 'store'])->name('store');
+    Route::get('/discover',[SetController::class,'discover'])->name('discover');
+    Route::get('/show/',[SetController::class,'show'])->name('show');
 });
+
+//Route::middleware('auth') -> name('sets.') -> prefix('sets') -> group(function(){
+//    Route::get('/create/{id}',[SetController::class,'create'])->name('create'); //sets.index
+//});
+
+Route::middleware('auth') -> name('profiles.') -> prefix('profiles') -> group(function(){
+    Route::get('/index',[UserController::class,'index'])->name('index');
+    Route::get('/showMyProfile',[UserController::class,'showMyProfile'])->name('showMyProfile');
+});
+
 
 require __DIR__.'/auth.php';
